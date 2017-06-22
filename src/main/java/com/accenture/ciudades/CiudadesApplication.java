761dd31dcd16c3df156b2bd4340dd4d450078187
SaraPavas <sara.pavas@udea.edu.co>
2017-06-22 16:40:43 -0500

    Inicial

---
+++ src/main/java/com/accenture/ciudades/CiudadesApplication.java
@@ -0,0 +1,12 @@
+package com.accenture.ciudades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiudadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiudadesApplication.class, args);
+	}
+}
